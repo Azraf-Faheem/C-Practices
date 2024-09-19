b9ae4332e24deeccf76e7d4d1a6f2f1937cb58de
Azraf-Faheem <azraf.faheem.42152@gmail.com>
2024-09-20 01:10:26 +0600

    Switch case added

---
+++ MenuDrivenProgram.c
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int option;
+    cout<"1. Convert Celsius to Fahrenheit \n";
+    cout<<"2. Convert Kilometers to Miles \n";
+    cout<<"3. Find the area of a Circle \n";
+    cin>>option;
+    switch(option)
+    case:1
+    {
+
+    }
+    case:2
+    {
+
+    }
+    case:3
+    {
+
+    }
+    default:
+        {
+            cout<<" You can only choose between 1 to 3";
+        }
+}
---
+++ MenuDrivenProgram.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int option;
+    cout<<"1. Convert Celsius to Fahrenheit \n";
+    cout<<"2. Convert Kilometers to Miles \n";
+    cout<<"3. Find the area of a Circle \n";
+    cin>>option;
+    switch (option)
+    {
+    case 1:
+        {
+
+    float c; // temperature in celsius
+    cout<<"Enter Temperature in Celsius: \n";
+    cin>>c;
+    float convert=((9/5)*c)+32;
+    cout<<"Temperature in Fahrenheit is : "<<convert<<endl;
+    break;
+
+         }
+    case 2:
+        {
+            float kilo;
+    cout<<"Enter value in Kilometers: \n";
+    cin>>kilo;
+    float Miles=(kilo/0.621);
+    cout<<" In miles it is :"<<Miles<<" miles"<<endl;
+    break;
+        }
+
+    case 3:
+    {
+    float area,radius;
+    cout<< "Enter the radius of the Circle: \n";
+    cin>>radius;
+    area= 2*3.1416*radius;
+    cout<< "The area of the circle which has radius of "<<radius<<" meters is : "<<area<<" sqmt";
+    break;
+    }
+
+    default:
+        cout<<" You can only choose between 1 to 3";
+    }
+
+}
