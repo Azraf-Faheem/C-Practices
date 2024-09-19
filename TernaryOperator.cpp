37c3de04124b7a46f134aeb0e4f6cf9a205bc740
Azraf Faheem <azraf.faheem.42152@gmail.com>
2024-09-20 01:52:13 +0600

    Update TernaryOperator.cpp
    problem fixed

--- TernaryOperator.cpp
+++ TernaryOperator.cpp
@@ -4,7 +4,7 @@ int main()
 {
     int a,b,c;
     cout<<"Input Three Numbers: \n";
-    cin>>a>>b>>c;
-    string Highest=(a>b&&a>c)?"The greatest is":"no";
-    cout<<Highest;
+    cin>>a >> b >> c;
+    string Highest=(a>b && a>c)?a : ((b>a && b>c)?b :c); //using ternaryy operator to write the conditional expression
+    cout<<"The Greatest Number of the inserted three numbers is : "<<Highest;
 }
