eda905feac40402fad0f69356a1faf7d8b391c1e
Azraf Faheem <azraf.faheem.42152@gmail.com>
2024-09-20 01:53:51 +0600

    Update TernaryOperator.cpp

--- TernaryOperator.cpp
+++ TernaryOperator.cpp
@@ -5,6 +5,6 @@ int main()
     int a,b,c;
     cout<<"Input Three Numbers: \n";
     cin>>a >> b >> c;
-    string Highest=(a>b && a>c)?a : ((b>a && b>c)?b :c); //using ternaryy operator to write the conditional expression
+    int Highest=(a>b && a>c)?a : ((b>a && b>c)?b :c); //using ternaryy operator to write the conditional expression
     cout<<"The Greatest Number of the inserted three numbers is : "<<Highest;
 }
